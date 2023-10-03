772828ccae9401d2713957b64b187eaa4f02c154
AJ Sorbello <ajsorbello@gmail.com>
2023-10-03 16:58:05 -0700

    created pokemon list of height

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,13 @@
-pokemonList= [
+let pokemonList = [
     { name: "Pikachu", height: .4, type: ["electric", "speed"] },
-    { name: "Blastoise", height: 1.6, type: ["water", "cannon"]},
+    { name: "Blastoise", height: 1.6, type: ["water", "cannon"] },
     { name: "Sandslash", height: 1, type: ["ground", "slow"] }
 ];
+for (let i = 0; i < pokemonList.length; i++) {
+    if (pokemonList[i].height > 1) {
+        document.write(pokemonList[i].name + "  " + "(height:" + "  " + pokemonList[i].height + " ) " + "- Wow, That\'s one big pokemon!" + "<br>"); 
+//condition for pokemon with the height > 1 
+    } else {
+        document.write(pokemonList[i].name + "  " + "(height:" + "  " + pokemonList[i].height + " ) " + "<br>");
+    }
+}
